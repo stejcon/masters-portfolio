ea3333db135fdb4fc1910ee1c372b5ea535d5f2b
Stephen Condon <stephen.condon2@gmail.com>
2024-01-04 00:07:11 +0000

    Update flake so you dont need to build torch from scartch to get cuda

--- flake.nix
+++ flake.nix
@@ -8,15 +8,13 @@
     nixpkgs,
   }: let
     system = "x86_64-linux";
-    # NOTE: If torch with cuda takes too long to build (took 6 hours with an i7 12th gen laptop) change cuda support to false
     pkgs = import nixpkgs { inherit system; config.allowUnfree = true; config.cudaSupport=true; };
-    pyEnv = pkgs.python3.withPackages(ps: with ps; [ torch torchvision onnx numpy pillow matplotlib jupyter ipython ]);
+    pyEnv = pkgs.python3.withPackages(ps: with ps; [ torchvision-bin onnx numpy pillow matplotlib jupyter ipython ]);
   in {
     formatter.${system} = pkgs.alejandra;
     devShells.${system} = {
       default = pkgs.mkShell {
-        packages = [ pyEnv ];
-        shellHook = "jupyter notebook";
+        packages = [ pyEnv pkgs.typst ];
       };
     };
   };
