3f1705085bf01d1097a62bcfb76809c1f9cbf9c3
Stephen Condon <stephen.condon2@gmail.com>
2024-01-18 18:01:53 +0000

    do inference time test for all resnet on cifar10

--- default-resnet152-inference-time-test.py
+++ default-resnet152-inference-time-test.py
@@ -1,6 +1,6 @@
 import torch
 import torchvision.transforms as transforms
-from torchvision.models import resnet152
+from torchvision.models import resnet18, resnet34, resnet50, resnet101, resnet152
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 import time
@@ -41,7 +41,13 @@ def load_cifar10(batch_size):
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Load ResNet152 model
-resnet152_model = resnet152(pretrained=True).to(device)
+resnet18_model = resnet50().to(device)
+resnet34_model = resnet50().to(device)
+resnet50_model = resnet50().to(device)
+resnet101_model = resnet50().to(device)
+resnet152_model = resnet152().to(device)
+
+models = [model.to(device) for model in [resnet18(), resnet34(), resnet50(), resnet101(), resnet152()]]
 
 # Specify batch size
 batch_size = 64
@@ -49,6 +55,6 @@ batch_size = 64
 # Load datasets
 cifar10_dataloader = load_cifar10(batch_size)
 
-# Measure average inference time for CIFAR-10
-cifar10_avg_time = measure_inference_time(resnet152_model, cifar10_dataloader, device)
-print(f'Average Inference Time on CIFAR-10: {cifar10_avg_time:.4f} seconds')
+inferenceTimes = [measure_inference_time(model, cifar10_dataloader, device) for model in models]
+for iTime in inferenceTimes:
+    print(f'Average Inference Time on CIFAR-10: {iTime:.4f} seconds')
