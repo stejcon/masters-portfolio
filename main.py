af6ff4be79f647958fae2391eef6b880c917374a
Stephen Condon <stephen.condon2@gmail.com>
2024-01-23 13:32:38 +0000

    readd full and half model for comparison

--- main.py
+++ main.py
@@ -142,23 +142,23 @@ def main():
     device = helpers.getDevice()
     trainLoader, validLoader, testLoader = helpers.Cifar10Splits()
 
-    # fullModel = ResNet(ResidualBlock, [3, 4, 6, 3]).to(device).train()
-    # helpers.trainModel(fullModel, trainLoader, validLoader, testLoader)
-    # fullModel.eval()
-    # torch.save(fullModel.state_dict(), "models/fullModel")
+    fullModel = ResNet(ResidualBlock, [3, 4, 6, 3]).to(device).train()
+    helpers.trainModel(fullModel, trainLoader, validLoader, testLoader)
+    fullModel.eval()
+    torch.save(fullModel.state_dict(), "models/fullModel")
 
-    # halfModel = HalfResNet(ResidualBlock, [3, 4, 6, 3]).to(device).train()
-    # helpers.trainModel(halfModel, trainLoader, validLoader, testLoader)
-    # halfModel.eval()
-    # torch.save(halfModel.state_dict(), "models/halfModel")
+    halfModel = HalfResNet(ResidualBlock, [3, 4, 6, 3]).to(device).train()
+    helpers.trainModel(halfModel, trainLoader, validLoader, testLoader)
+    halfModel.eval()
+    torch.save(halfModel.state_dict(), "models/halfModel")
 
     branchedModel = BranchedResNet(ResidualBlock, [3, 4, 6, 3]).to(device).train()
     helpers.trainModel(branchedModel, trainLoader, validLoader, testLoader)
     branchedModel.eval()
     torch.save(branchedModel.state_dict(), "models/branchedModel")
 
-    # helpers.generateJsonResults(fullModel, "fullModel", testLoader)
-    # helpers.generateJsonResults(halfModel, "halfModel", testLoader)
+    helpers.generateJsonResults(fullModel, "fullModel", testLoader)
+    helpers.generateJsonResults(halfModel, "halfModel", testLoader)
     helpers.generateJsonResults(branchedModel, "branchedModel", testLoader)
 
 if __name__ == "__main__":
