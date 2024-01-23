0185d1d62f17789e6b0d3b81e49ab64845ed196c
Stephen Condon <stephen.condon2@gmail.com>
2024-01-23 13:32:14 +0000

    add BranchedResNet with training

--- main.py
+++ main.py
@@ -84,22 +84,82 @@ class HalfResNet(ResNet):
         x = self.linear(x)
         return x
 
+class BranchedResNet(nn.Module):
+    def __init__(self, block, layers, num_classes = 10):
+        super(BranchedResNet, self).__init__()
+        self.inplanes = 64
+        self.conv1 = nn.Sequential(
+                        nn.Conv2d(3, 64, kernel_size = 7, stride = 2, padding = 3),
+                        nn.BatchNorm2d(64),
+                        nn.ReLU())
+        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
+        self.layer0 = self._make_layer(block, 64, layers[0], stride = 1)
+        self.layer1 = self._make_layer(block, 128, layers[1], stride = 2)
+        self.layer2 = self._make_layer(block, 256, layers[2], stride = 2)
+        self.layer3 = self._make_layer(block, 512, layers[3], stride = 2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512, num_classes)
+        self.exit = nn.Linear(61952, num_classes)
+        
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.layer0(x)
+        x = self.layer1(x)
+
+        y = self.avgpool(x)
+        y = y.view(x.size(0), -1)
+        y = self.exit(y)
+        y = torch.nn.functional.softmax(y, dim=1)
+        entropy = -torch.sum(y * torch.log2(y + 1e-20), dim=1)
+
+        if(torch.all(entropy < 0.5)):
+            return y
+
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
 def main():
     device = helpers.getDevice()
     trainLoader, validLoader, testLoader = helpers.Cifar10Splits()
 
-    fullModel = ResNet(ResidualBlock, [3, 4, 6, 3]).to(device).train()
-    helpers.trainModel(fullModel, trainLoader, validLoader, testLoader)
-    fullModel.eval()
-    torch.save(fullModel.state_dict(), "models/fullModel")
+    # fullModel = ResNet(ResidualBlock, [3, 4, 6, 3]).to(device).train()
+    # helpers.trainModel(fullModel, trainLoader, validLoader, testLoader)
+    # fullModel.eval()
+    # torch.save(fullModel.state_dict(), "models/fullModel")
 
-    halfModel = HalfResNet(ResidualBlock, [3, 4, 6, 3]).to(device).train()
-    helpers.trainModel(halfModel, trainLoader, validLoader, testLoader)
-    halfModel.eval()
-    torch.save(halfModel.state_dict(), "models/halfModel")
+    # halfModel = HalfResNet(ResidualBlock, [3, 4, 6, 3]).to(device).train()
+    # helpers.trainModel(halfModel, trainLoader, validLoader, testLoader)
+    # halfModel.eval()
+    # torch.save(halfModel.state_dict(), "models/halfModel")
 
-    helpers.generateJsonResults(fullModel, "fullModel", testLoader)
-    helpers.generateJsonResults(halfModel, "halfModel", testLoader)
+    branchedModel = BranchedResNet(ResidualBlock, [3, 4, 6, 3]).to(device).train()
+    helpers.trainModel(branchedModel, trainLoader, validLoader, testLoader)
+    branchedModel.eval()
+    torch.save(branchedModel.state_dict(), "models/branchedModel")
+
+    # helpers.generateJsonResults(fullModel, "fullModel", testLoader)
+    # helpers.generateJsonResults(halfModel, "halfModel", testLoader)
+    helpers.generateJsonResults(branchedModel, "branchedModel", testLoader)
 
 if __name__ == "__main__":
     main()
