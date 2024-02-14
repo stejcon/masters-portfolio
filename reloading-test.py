f4e5dbf935792da8c22827b5aee0e1b39b8a40d5
Stephen Condon <stephen.condon2@gmail.com>
2024-02-14 21:15:03 +0000

    remove some unneeded files

--- reloading-test.py
+++
@@ -1,8 +0,0 @@
-import unparsing
-import testclass
-
-obj = testclass.MyClass(42)
-obj.my_method()
-unparsing.main()
-obj = testclass.MyClass(42)
-obj.my_method()
--- testclass.py
+++
@@ -1,7 +0,0 @@
-class MyClass:
-
-    def __init__(self, value):
-        self.value = value
-
-    def my_method(self):
-        print('Modified method:', self.value + 10)
