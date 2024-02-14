078019f8929bd3fe75b8648f2973912872c0702f
Stephen Condon <stephen.condon2@gmail.com>
2024-02-14 13:48:06 +0000

    files to test unparsing and saving

---
+++ reloading-test.py
@@ -0,0 +1,8 @@
+import unparsing
+import testclass
+
+obj = testclass.MyClass(42)
+obj.my_method()
+unparsing.main()
+obj = testclass.MyClass(42)
+obj.my_method()
---
+++ testclass.py
@@ -0,0 +1,7 @@
+class MyClass:
+
+    def __init__(self, value):
+        self.value = value
+
+    def my_method(self):
+        print('Modified method:', self.value + 10)
