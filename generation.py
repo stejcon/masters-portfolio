7d3f813ed01ce3ec126ec206c2258821050840b2
Stephen Condon <stephen.condon2@gmail.com>
2024-02-19 16:01:18 +0000

    reformat

--- generation.py
+++ generation.py
@@ -1,11 +1,32 @@
 import ast
-from ast import Assign, FunctionDef, Name, Store, Call, Attribute, Load, Constant, UnaryOp, USub, keyword, BinOp, Mult, Return, Tuple, Lt, Compare, If, Add, Module
+from ast import (
+    Assign,
+    FunctionDef,
+    Name,
+    Store,
+    Call,
+    Attribute,
+    Load,
+    Constant,
+    UnaryOp,
+    USub,
+    keyword,
+    BinOp,
+    Mult,
+    Return,
+    Tuple,
+    Lt,
+    Compare,
+    If,
+    Add,
+)
 import inspect
 import textwrap
-import torch # Ignore LSP error "not accessed", needed to compile the ast code
-import torch.nn as nn # Ignore LSP error "not accessed", needed to compile the ast code
+import torch
+import torch.nn as nn
 
-class EarlyExit():
+
+class EarlyExit:
     def __init__(self, node, threshold, id):
         self.node = node
         self.setThreshold(threshold)
@@ -21,7 +42,12 @@ class EarlyExit():
         def edit_exit_threshold(nodes, threshold):
             edited_nodes = []
             for node in nodes:
-                if isinstance(node, ast.If) and isinstance(node.test, ast.Call) and isinstance(node.test.args[0], ast.Compare) and isinstance(node.test.args[0].comparators[0], ast.Constant):
+                if (
+                    isinstance(node, ast.If)
+                    and isinstance(node.test, ast.Call)
+                    and isinstance(node.test.args[0], ast.Compare)
+                    and isinstance(node.test.args[0].comparators[0], ast.Constant)
+                ):
                     node.test.args[0].comparators[0] = ast.Constant(value=threshold)
                 edited_nodes.append(node)
             return edited_nodes
@@ -39,7 +65,12 @@ class EarlyExit():
         def edit_return_id(nodes, new_id):
             edited_nodes = []
             for node in nodes:
-                if isinstance(node, ast.If) and isinstance(node.body[0], ast.Return) and isinstance(node.body[0].value, ast.Tuple) and isinstance(node.body[0].value.elts[0], ast.Constant):
+                if (
+                    isinstance(node, ast.If)
+                    and isinstance(node.body[0], ast.Return)
+                    and isinstance(node.body[0].value, ast.Tuple)
+                    and isinstance(node.body[0].value.elts[0], ast.Constant)
+                ):
                     node.body[0].value.elts[0] = ast.Constant(value=new_id)
                 edited_nodes.append(node)
             return edited_nodes
@@ -50,18 +81,33 @@ class EarlyExit():
         self.id = id
         self.updateReturn()
 
-getAstFromSource = lambda x: ast.parse(textwrap.dedent(inspect.getsource(x)))
-getAstDump = lambda x: ast.dump(x, indent=4)
+
+def getAstFromSource(x):
+    ast.parse(textwrap.dedent(inspect.getsource(x)))
+
+
+def getAstDump(x):
+    ast.dump(x, indent=4)
+
 
 class ExitTracker:
     # TODO: Make the model taken in here a ReloadableModel
     def __init__(self, model, accuracy):
         self.targetAccuracy = accuracy
-        self.model = model
-        self.original_ast = getAstFromSource(self.model.forward)
-        self.current_ast = getAstFromSource(self.model.forward)
-        assert(isinstance(self.current_ast.body[0], FunctionDef))
-        self.current_list = [x for x in self.current_ast.body[0].body]
+        self.reloadable_model = model
+        self.model = self.reloadable_model.getModel()
+        self.ff_body_ast = getAstFromSource(self.model.forward)
+        assert isinstance(self.ff_body_ast.body[0], FunctionDef)
+        self.ff_node_list = [x for x in self.ff_body_ast.body[0].body]
+
+        self.exits = []
+        for i in range(len(self.ff_node_list)):
+            self.exits.append(EarlyExit(exitAst, 30, i + 1))
+
+        self.updated_forward_function_node_list = []
+        for i in range(len(self.exits)):
+            self.updated_forward_function_node_list.append(self.ff_node_list[i])
+            self.updated_forward_function_node_list.append(self.exits[i])
 
     # TODO: Function to test exit and set entropy threshold correctly
 
@@ -70,26 +116,23 @@ class ExitTracker:
     # TODO: Function to add a new exit and disable other exits
 
     def transformFunction(self):
-        self.prev_ast = self.current_ast
-        self.current_list.insert(3, EarlyExit(exitAst, 37, 6))
+        self.ff_node_list.insert(3, EarlyExit(exitAst, 37, 6))
 
         b = []
-        for x in self.current_list:
-            if (isinstance(x, EarlyExit)):
+        for x in self.ff_node_list:
+            if isinstance(x, EarlyExit):
                 b.extend(x.node)
             else:
                 b.append(x)
 
-        assert(isinstance(self.current_ast.body[0], FunctionDef))
-        self.current_ast.body[0].body = b
-        ast.fix_missing_locations(self.current_ast)
+        self.ff_body_ast.body[0].body = b
+        ast.fix_missing_locations(self.ff_body_ast)
 
-        # TODO: Instead of recompileForward, write the updated module and reload
-        # self.recompileForward()
         self.saveForwardFunction()
+        self.model = self.reloadable_model.reload()
 
     def saveForwardFunction(self):
-        filePath: str = inspect.getmodule(self.model).__file__ # type: ignore
+        filePath = inspect.getmodule(self.model).__file__
         class_name = self.model.__class__.__name__
         method_name = self.model.forward.__name__
 
@@ -105,10 +148,13 @@ class ExitTracker:
             if isinstance(node, ast.ClassDef) and node.name == class_name:
                 # Find the existing method node
                 for body_item in node.body:
-                    if isinstance(body_item, ast.FunctionDef) and body_item.name == method_name:
+                    if (
+                        isinstance(body_item, ast.FunctionDef)
+                        and body_item.name == method_name
+                    ):
                         # Replace the method node with the new AST
                         method_index = node.body.index(body_item)
-                        node.body[method_index] = self.current_ast
+                        node.body[method_index] = self.ff_body_ast
                         break
 
         # Convert the modified AST back to code
@@ -118,203 +164,197 @@ class ExitTracker:
         with open(filePath, "w") as file:
             file.write(modified_code)
 
-    # TODO: Put this as the other ast stuff into a separate appendix, not applicable anymore
+    # TODO: Put this as the other runtime ast stuff into appendix
     def recompileForward(self):
-        code_object = compile(self.current_ast, '', 'exec')
+        code_object = compile(self.ff_body_ast, "", "exec")
         exec(code_object, globals())
         bound_method = globals()["forward"].__get__(self.model, self.model.__class__)
-        setattr(self.model, 'forward', bound_method)
+        setattr(self.model, "forward", bound_method)
+
 
 class AddExitTransformer(ast.NodeTransformer):
     def __init__(self, astlist):
         self.exitAst = astlist
 
     def visit_Assign(self, node):
-        if isinstance(node, ast.Assign) and isinstance(node.value, ast.Call) and isinstance(node.value.func, ast.Attribute) and node.value.func.attr == "layer1":
+        if (
+            isinstance(node, ast.Assign)
+            and isinstance(node.value, ast.Call)
+            and isinstance(node.value.func, ast.Attribute)
+            and node.value.func.attr == "layer1"
+        ):
             return [node] + self.exitAst
         return node
+
 
 class EditExitThreshold(ast.NodeTransformer):
     def __init__(self, threshold):
         self.threshold = threshold
 
     def visit_If(self, node):
-        if isinstance(node, If) and isinstance(node.test, Call) and isinstance(node.test.args[0], Compare) and isinstance(node.test.args[0].comparators[0], Constant):
+        if (
+            isinstance(node, If)
+            and isinstance(node.test, Call)
+            and isinstance(node.test.args[0], Compare)
+            and isinstance(node.test.args[0].comparators[0], Constant)
+        ):
             node.test.args[0].comparators[0] = Constant(value=self.threshold)
             return node
         return node
+
 
 class EditReturnId(ast.NodeTransformer):
     def __init__(self, id):
         self.id = id
 
     def visit_If(self, node):
-        if isinstance(node, If) and isinstance(node.body[0], Return) and isinstance(node.body[0].value, Tuple) and isinstance(node.body[0].value.elts[0], Constant):
+        if (
+            isinstance(node, If)
+            and isinstance(node.body[0], Return)
+            and isinstance(node.body[0].value, Tuple)
+            and isinstance(node.body[0].value.elts[0], Constant)
+        ):
             node.body[0].value.elts[0] = Constant(value=self.id)
             return node
         return node
 
+
 exitAst = [
     Assign(
-        targets=[Name(id='y', ctx=Store())],
+        targets=[Name(id="y", ctx=Store())],
         value=Call(
             func=Attribute(
-                value=Name(id='self', ctx=Load()),
-                attr='avgpool',
-                ctx=Load()
+                value=Name(id="self", ctx=Load()), attr="avgpool", ctx=Load()
             ),
-            args=[Name(id='x', ctx=Load())],
-            keywords=[]
-        )
+            args=[Name(id="x", ctx=Load())],
+            keywords=[],
+        ),
     ),
-
     Assign(
-        targets=[Name(id='y', ctx=Store())],
+        targets=[Name(id="y", ctx=Store())],
         value=Call(
-            func=Attribute(
-                value=Name(id='y', ctx=Load()),
-                attr='view',
-                ctx=Load()
-            ),
+            func=Attribute(value=Name(id="y", ctx=Load()), attr="view", ctx=Load()),
             args=[
                 Call(
                     func=Attribute(
-                        value=Name(id='x', ctx=Load()),
-                        attr='size',
-                        ctx=Load()
+                        value=Name(id="x", ctx=Load()), attr="size", ctx=Load()
                     ),
                     args=[Constant(value=0)],
-                    keywords=[]
+                    keywords=[],
                 ),
-                UnaryOp(
-                    op=USub(),
-                    operand=Constant(value=1)
-                )
-            ], 
-            keywords=[]
-        )
+                UnaryOp(op=USub(), operand=Constant(value=1)),
+            ],
+            keywords=[],
+        ),
     ),
-
     Assign(
-        targets=[Name(id='y', ctx=Store())],
+        targets=[Name(id="y", ctx=Store())],
         value=Call(
             func=Call(
                 func=Attribute(
                     value=Attribute(
-                        value=Name(id='torch', ctx=Load()),
-                        attr='nn',
-                        ctx=Load()
+                        value=Name(id="torch", ctx=Load()),
+                        attr="nn",
+                        ctx=Load(),
                     ),
-                    attr='Linear',
-                    ctx=Load()
+                    attr="Linear",
+                    ctx=Load(),
                 ),
                 args=[
                     Call(
                         func=Attribute(
-                            value=Name(id='y', ctx=Load()),
-                            attr='size',
-                            ctx=Load()
+                            value=Name(id="y", ctx=Load()),
+                            attr="size",
+                            ctx=Load(),
                         ),
                         args=[Constant(value=1)],
-                        keywords=[]
+                        keywords=[],
                     ),
                     Attribute(
-                        value=Name(id='self', ctx=Load()),
-                        attr='num_classes',
-                        ctx=Load()
-                    )
+                        value=Name(id="self", ctx=Load()),
+                        attr="num_classes",
+                        ctx=Load(),
+                    ),
                 ],
-                keywords=[]
+                keywords=[],
             ),
-            args=[Name(id='y', ctx=Load())],
-            keywords=[]
-        )
+            args=[Name(id="y", ctx=Load())],
+            keywords=[],
+        ),
     ),
-
     Assign(
-        targets=[Name(id='y', ctx=Store())],
+        targets=[Name(id="y", ctx=Store())],
         value=Call(
             func=Attribute(
                 value=Attribute(
                     value=Attribute(
-                        value=Name(id='torch', ctx=Load()),
-                        attr='nn',
-                        ctx=Load()
+                        value=Name(id="torch", ctx=Load()),
+                        attr="nn",
+                        ctx=Load(),
                     ),
-                    attr='functional',
-                    ctx=Load()
+                    attr="functional",
+                    ctx=Load(),
                 ),
-                attr='softmax',
-                ctx=Load()
+                attr="softmax",
+                ctx=Load(),
             ),
-            args=[Name(id='y', ctx=Load())],
-            keywords=[keyword(arg='dim', value=Constant(value=1))]
-        )
+            args=[Name(id="y", ctx=Load())],
+            keywords=[keyword(arg="dim", value=Constant(value=1))],
+        ),
     ),
-
     Assign(
-        targets=[Name(id='entropy', ctx=Store())],
+        targets=[Name(id="entropy", ctx=Store())],
         value=UnaryOp(
             op=USub(),
             operand=Call(
                 func=Attribute(
-                    value=Name(id='torch', ctx=Load()),
-                    attr='sum',
-                    ctx=Load()
+                    value=Name(id="torch", ctx=Load()), attr="sum", ctx=Load()
                 ),
                 args=[
                     BinOp(
-                        left=Name(id='y', ctx=Load()),
+                        left=Name(id="y", ctx=Load()),
                         op=Mult(),
                         right=Call(
                             func=Attribute(
-                                value=Name(id='torch', ctx=Load()),
-                                attr='log2',
-                                ctx=Load()
+                                value=Name(id="torch", ctx=Load()),
+                                attr="log2",
+                                ctx=Load(),
                             ),
                             args=[
                                 BinOp(
-                                    left=Name(id='y', ctx=Load()),
+                                    left=Name(id="y", ctx=Load()),
                                     op=Add(),
-                                    right=Constant(value=1e-20)
+                                    right=Constant(value=1e-20),
                                 )
                             ],
-                            keywords=[]
-                        )
+                            keywords=[],
+                        ),
                     )
                 ],
-                keywords=[keyword(arg='dim', value=Constant(value=1))]
-            )
-        )
+                keywords=[keyword(arg="dim", value=Constant(value=1))],
+            ),
+        ),
     ),
-
     If(
         test=Call(
-            func=Attribute(
-                value=Name(id='torch', ctx=Load()),
-                attr='all',
-                ctx=Load()
-            ),
+            func=Attribute(value=Name(id="torch", ctx=Load()), attr="all", ctx=Load()),
             args=[
                 Compare(
-                    left=Name(id='entropy', ctx=Load()),
+                    left=Name(id="entropy", ctx=Load()),
                     ops=[Lt()],
-                    comparators=[Constant(value=0)]
+                    comparators=[Constant(value=0)],
                 )
             ],
-            keywords=[]
+            keywords=[],
         ),
         body=[
             Return(
                 value=Tuple(
-                    elts=[
-                        Constant(value=1),
-                        Name(id='y', ctx=Load())
-                    ],
-                    ctx=Load()
+                    elts=[Constant(value=1), Name(id="y", ctx=Load())],
+                    ctx=Load(),
                 )
             )
         ],
-        orelse=[]
+        orelse=[],
     ),
 ]
