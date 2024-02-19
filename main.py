7e99e71dcc84b1d70056697d744fcafd1e41867b
Stephen Condon <stephen.condon2@gmail.com>
2024-02-19 17:00:32 +0000

    redo main to only do a full reloadable model

--- main.py
+++ main.py
@@ -4,29 +4,15 @@ import models
 
 
 def main():
-    device = helpers.getDevice()
     helpers.createModelsFolder("models")
     trainLoader, validLoader, testLoader = helpers.Cifar10Splits()
 
-    fullModel = models.ResNet(models.ResidualBlock, [3, 4, 6, 3]).to(device).train()
-    helpers.trainModelWithBranch(fullModel, trainLoader, validLoader, testLoader)
-    fullModel.eval()
-    torch.save(fullModel.state_dict(), "models/fullModel")
-    helpers.generateJsonResults(fullModel, "fullModel", testLoader)
-
-    halfModel = models.HalfResNet(models.ResidualBlock, [3, 4, 6, 3]).to(device).train()
-    helpers.trainModel(halfModel, trainLoader, validLoader, testLoader)
-    halfModel.eval()
-    torch.save(halfModel.state_dict(), "models/halfModel")
-    helpers.generateJsonResults(halfModel, "halfModel", testLoader)
-
-    branchedModel = (
-        models.BranchedResNet(models.ResidualBlock, [3, 4, 6, 3]).to(device).train()
+    fullModel = helpers.ReloadableModel(
+        models.ResNet, models.ResidualBlock, [3, 4, 6, 3]
     )
-    helpers.trainModel(branchedModel, trainLoader, validLoader, testLoader)
-    branchedModel.eval()
-    torch.save(branchedModel.state_dict(), "models/branchedModel")
-    helpers.generateJsonResults(branchedModel, "branchedModel", testLoader)
+    helpers.trainModelWithBranch(fullModel, trainLoader, validLoader, testLoader)
+    torch.save(fullModel.getModel().state_dict(), "models/fullModel")
+    helpers.generateJsonResults(fullModel.getModel(), "fullModel", testLoader)
 
 
 if __name__ == "__main__":
