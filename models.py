7c57a552e9e65b886b3d7bb11ff5cb4eda7155bc
Stephen Condon <stephen.condon2@gmail.com>
2024-02-20 11:35:19 +0000

    Revert "updated models"
    This reverts commit 77663e16b33026e60be9898389164f0707521265.

--- models.py
+++ models.py
@@ -1,12 +1,21 @@
 import torch
 import torch.nn as nn
 
-class ResidualBlock(nn.Module):
 
+class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super().__init__()
-        self.conv1 = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1), nn.BatchNorm2d(out_channels), nn.ReLU())
-        self.conv2 = nn.Sequential(nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1), nn.BatchNorm2d(out_channels))
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels, out_channels, kernel_size=3, stride=stride, padding=1
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels),
+        )
         self.downsample = downsample
         self.relu = nn.ReLU()
         self.out_channels = out_channels
@@ -21,12 +30,16 @@ class ResidualBlock(nn.Module):
         out = self.relu(out)
         return out
 
-class ResNet(nn.Module):
 
+class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes=10):
         super().__init__()
         self.inplanes = 64
-        self.conv1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3), nn.BatchNorm2d(64), nn.ReLU())
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer0 = self._make_layer(block, 64, layers[0], stride=1)
         self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
@@ -39,7 +52,10 @@ class ResNet(nn.Module):
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes:
-            downsample = nn.Sequential(nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride), nn.BatchNorm2d(planes))
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes),
+            )
         layers = []
         layers.append(block(self.inplanes, planes, stride, downsample))
         self.inplanes = planes
@@ -59,8 +75,8 @@ class ResNet(nn.Module):
         x = self.fc(x)
         return (0, x)
 
-class HalfResNet(ResNet):
 
+class HalfResNet(ResNet):
     def __init__(self, block, layers, num_classes=10):
         super(HalfResNet, self).__init__(block, layers, num_classes=10)
         self.linear = nn.Linear(61952, num_classes)
@@ -71,24 +87,21 @@ class HalfResNet(ResNet):
         x = self.maxpool(x)
         x = self.layer0(x)
         x = self.layer1(x)
-        y = self.avgpool(x)
-        y = y.view(x.size(0), -1)
-        y = torch.nn.Linear(y.size(1), self.num_classes)(y)
-        y = torch.nn.functional.softmax(y, dim=1)
-        entropy = -torch.sum(y * torch.log2(y + 1e-20), dim=1)
-        if torch.all(entropy < 3.3219253957271575):
-            return (1, y)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.linear(x)
         return (0, x)
 
-class BranchedResNet(nn.Module):
 
+class BranchedResNet(nn.Module):
     def __init__(self, block, layers, num_classes=10):
         super(BranchedResNet, self).__init__()
         self.inplanes = 64
-        self.conv1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3), nn.BatchNorm2d(64), nn.ReLU())
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer0 = self._make_layer(block, 64, layers[0], stride=1)
         self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
@@ -101,7 +114,10 @@ class BranchedResNet(nn.Module):
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes:
-            downsample = nn.Sequential(nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride), nn.BatchNorm2d(planes))
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes),
+            )
         layers = []
         layers.append(block(self.inplanes, planes, stride, downsample))
         self.inplanes = planes
