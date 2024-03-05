011e2f7976e015ee0aeab7905ebc09bdb3d6df00
Stephen Condon <stephen.condon2@gmail.com>
2024-03-05 20:20:01 +0000

    update models

--- models.py
+++ models.py
@@ -77,6 +77,65 @@ class BaseResNet(nn.Module):
         return (0, x)
 
 
+class StrippedResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        super().__init__()
+        self.inplanes = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer0 = self._make_layer(block, 64, layers[0], stride=1)
+        self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer2 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer3 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512, num_classes)
+        self.num_classes = num_classes
+        self.exit5 = torch.nn.LazyLinear(self.num_classes)
+        self.exit6 = torch.nn.LazyLinear(self.num_classes)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        y = self.avgpool(x)
+        y = y.view(x.size(0), -1)
+        y = self.exit5(y)
+        with torch.no_grad():
+            pk = F.softmax(y, dim=1)
+            entr = -torch.sum(pk * torch.log(pk + 1e-20))
+            if torch.all(entr < 0.51512845993042):
+                return (5, y)
+        x = self.layer3(x)
+        y = self.avgpool(x)
+        y = y.view(x.size(0), -1)
+        y = self.exit6(y)
+        with torch.no_grad():
+            pk = F.softmax(y, dim=1)
+            entr = -torch.sum(pk * torch.log(pk + 1e-20))
+            if torch.all(entr < 300000):
+                return (6, y)
+
+
 class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes=10):
         super().__init__()
