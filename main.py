1cf351bc963b7e607db1ec7cf51ee66a2ee1e5a4
Stephen Condon <stephen.condon2@gmail.com>
2024-03-11 18:43:06 +0000

    change to BiggerExitResNet model

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def main():
     trainLoader, validLoader, testLoader = helpers.Cifar10Splits()
 
     fullModel = helpers.ReloadableModel(
-        models.ResNet, models.ResidualBlock, [3, 4, 6, 3]
+        models.BiggerExitResNet, models.ResidualBlock, [3, 4, 6, 3]
     )
     helpers.trainModelWithBranch(fullModel, trainLoader, validLoader, testLoader)
-    torch.save(fullModel.getModel().state_dict(), "models/fullModel")
+    torch.save(fullModel.getModel().state_dict(), "models/biggerExitModel")
 
     _, _, test = helpers.Cifar10Splits(1)
-    helpers.generateJsonResults(fullModel.getModel(), "fullModel", test)
+    helpers.generateJsonResults(fullModel.getModel(), "biggerExitModel", test)
 
 
 if __name__ == "__main__":
