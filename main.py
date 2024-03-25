17e63e68b6f1898ac46d77025f022ebc20546e09
Stephen Condon <stephen.condon2@gmail.com>
2024-03-25 01:12:05 +0000

    change main

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import helpers
 import torch
 import models
+from models import ResNet50QMNIST, ResNet50Cifar100, ResNet50Cifar10, ResNet50Fashion
+from models import (
+    ResNet101QMNIST,
+    ResNet101Cifar100,
+    ResNet101Cifar10,
+    ResNet101Fashion,
+)
+from models import (
+    ResNet152QMNIST,
+    ResNet152Cifar100,
+    ResNet152Cifar10,
+    ResNet152Fashion,
+)
 
 
 def main():
@@ -9,15 +22,29 @@ def main():
     resnet_names = ["50", "101", "152"]
     resnet_sizes = [[3, 4, 6, 3], [3, 4, 23, 3], [3, 8, 36, 3]]
     datasets = ["cifar10", "cifar100", "qmnist", "fashion-mnist"]
+    model_classes = [
+        ResNet50Cifar10,
+        ResNet50Cifar100,
+        ResNet50QMNIST,
+        ResNet50Fashion,
+        ResNet101Cifar10,
+        ResNet101Cifar100,
+        ResNet101QMNIST,
+        ResNet101Fashion,
+        ResNet152Cifar10,
+        ResNet152Cifar100,
+        ResNet152QMNIST,
+        ResNet152Fashion,
+    ]
 
-    for name, size in zip(resnet_names, resnet_sizes):
-        for dataset in datasets:
+    for i, (name, size) in enumerate(zip(resnet_names, resnet_sizes)):
+        for j, dataset in enumerate(datasets):
             trainLoader, validLoader, testLoader = helpers.get_custom_dataloaders(
                 dataset
             )
             _, _, test = helpers.get_custom_dataloaders(dataset, 1)
             model = helpers.ReloadableModel(
-                models.ResNet,
+                model_classes[i * len(resnet_names) + j],
                 models.Bottleneck,
                 size,
                 len(trainLoader.dataset.classes),
