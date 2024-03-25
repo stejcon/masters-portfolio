d29d11c0ee24a7bb995576313b158b25aa2907c1
Stephen Condon <stephen.condon2@gmail.com>
2024-03-25 00:44:07 +0000

    change model

--- models.py
+++ models.py
@@ -32,6 +32,68 @@ class ResidualBlock(nn.Module):
         return out
 
 
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    return nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=dilation,
+        groups=groups,
+        bias=False,
+        dilation=dilation,
+    )
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(
+        self,
+        inplanes,
+        planes,
+        stride=1,
+        downsample=None,
+        groups=1,
+        base_width=64,
+        dilation=1,
+        norm_layer=None,
+    ):
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.0)) * groups
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out += identity
+        out = self.relu(out)
+        return out
+
+
 class BaseResNet(nn.Module):
     def __init__(self, block, layers, num_classes=10):
         super().__init__()
@@ -94,10 +156,6 @@ class StrippedBiggerExitResNet(nn.Module):
         self.avgpool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512, num_classes)
         self.num_classes = num_classes
-        self.exitconv5 = nn.LazyConv2d(1, 1)
-        self.exitlin5 = nn.LazyLinear(self.num_classes)
-        self.exitconv6 = nn.LazyConv2d(1, 1)
-        self.exitlin6 = nn.LazyLinear(self.num_classes)
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
@@ -119,23 +177,7 @@ class StrippedBiggerExitResNet(nn.Module):
         x = self.layer0(x)
         x = self.layer1(x)
         x = self.layer2(x)
-        y = self.exitconv5(x)
-        y = torch.nn.Flatten()(y)
-        y = self.exitlin5(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 0.6140934772207401):
-                return (5, y)
         x = self.layer3(x)
-        y = self.exitconv6(x)
-        y = torch.nn.Flatten()(y)
-        y = self.exitlin6(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 1.026354412062792):
-                return (6, y)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
@@ -159,18 +201,6 @@ class BiggerExitResNet(nn.Module):
         self.avgpool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512, num_classes)
         self.num_classes = num_classes
-        self.exitconv1 = nn.LazyConv2d(1, 1)
-        self.exitlin1 = nn.LazyLinear(self.num_classes)
-        self.exitconv2 = nn.LazyConv2d(1, 1)
-        self.exitlin2 = nn.LazyLinear(self.num_classes)
-        self.exitconv3 = nn.LazyConv2d(1, 1)
-        self.exitlin3 = nn.LazyLinear(self.num_classes)
-        self.exitconv4 = nn.LazyConv2d(1, 1)
-        self.exitlin4 = nn.LazyLinear(self.num_classes)
-        self.exitconv5 = nn.LazyConv2d(1, 1)
-        self.exitlin5 = nn.LazyLinear(self.num_classes)
-        self.exitconv6 = nn.LazyConv2d(1, 1)
-        self.exitlin6 = nn.LazyLinear(self.num_classes)
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
@@ -188,217 +218,124 @@ class BiggerExitResNet(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
-        y = self.exitconv1(x)
-        y = torch.nn.Flatten()(y)
-        y = self.exitlin1(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 0.13258273735642434):
-                return (1, y)
         x = self.maxpool(x)
-        y = self.exitconv2(x)
-        y = torch.nn.Flatten()(y)
-        y = self.exitlin2(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 0.1333181261084974):
-                return (2, y)
         x = self.layer0(x)
-        y = self.exitconv3(x)
-        y = torch.nn.Flatten()(y)
-        y = self.exitlin3(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 0.47723102875053885):
-                return (3, y)
         x = self.layer1(x)
-        y = self.exitconv4(x)
-        y = torch.nn.Flatten()(y)
-        y = self.exitlin4(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 0.40452422946691513):
-                return (4, y)
         x = self.layer2(x)
-        y = self.exitconv5(x)
-        y = torch.nn.Flatten()(y)
-        y = self.exitlin5(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 0.6140934772207401):
-                return (5, y)
         x = self.layer3(x)
-        y = self.exitconv6(x)
-        y = torch.nn.Flatten()(y)
-        y = self.exitlin6(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 1.026354412062792):
-                return (6, y)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return (0, x)
 
 
-class StrippedResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=10):
-        super().__init__()
-        self.inplanes = 64
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-        )
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer0 = self._make_layer(block, 64, layers[0], stride=1)
-        self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer2 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer3 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(512, num_classes)
-        self.num_classes = num_classes
-        self.exit5 = torch.nn.LazyLinear(self.num_classes)
-        self.exit6 = torch.nn.LazyLinear(self.num_classes)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(planes),
-            )
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.maxpool(x)
-        x = self.layer0(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        y = self.avgpool(x)
-        y = y.view(x.size(0), -1)
-        y = self.exit5(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 0.51512845993042):
-                return (5, y)
-        x = self.layer3(x)
-        y = self.avgpool(x)
-        y = y.view(x.size(0), -1)
-        y = self.exit6(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 300000):
-                return (6, y)
-
-
 class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=10):
+    def __init__(
+        self,
+        block,
+        layers,
+        num_classes=1000,
+        zero_init_residual=False,
+        groups=1,
+        width_per_group=64,
+        replace_stride_with_dilation=None,
+        norm_layer=None,
+    ):
         super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
         self.inplanes = 64
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError(
+                f"replace_stride_with_dilation should be None or a 3-element tuple, got {replace_stride_with_dilation}"
+            )
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(
+            3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False
         )
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer0 = self._make_layer(block, 64, layers[0], stride=1)
-        self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer2 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer3 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(512, num_classes)
-        self.num_classes = num_classes
-        self.exit1 = torch.nn.LazyLinear(self.num_classes)
-        self.exit2 = torch.nn.LazyLinear(self.num_classes)
-        self.exit3 = torch.nn.LazyLinear(self.num_classes)
-        self.exit4 = torch.nn.LazyLinear(self.num_classes)
-        self.exit5 = torch.nn.LazyLinear(self.num_classes)
-        self.exit6 = torch.nn.LazyLinear(self.num_classes)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(
+            block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0]
+        )
+        self.layer3 = self._make_layer(
+            block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1]
+        )
+        self.layer4 = self._make_layer(
+            block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2]
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck) and m.bn3.weight is not None:
+                    nn.init.constant_(m.bn3.weight, 0)
 
-    def _make_layer(self, block, planes, blocks, stride=1):
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
         downsample = None
-        if stride != 1 or self.inplanes != planes:
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(planes),
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
             )
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes
+        layers.append(
+            block(
+                self.inplanes,
+                planes,
+                stride,
+                downsample,
+                self.groups,
+                self.base_width,
+                previous_dilation,
+                norm_layer,
+            )
+        )
+        self.inplanes = planes * block.expansion
         for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
+            layers.append(
+                block(
+                    self.inplanes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    norm_layer=norm_layer,
+                )
+            )
         return nn.Sequential(*layers)
 
     def forward(self, x):
         x = self.conv1(x)
-        y = self.avgpool(x)
-        y = y.view(x.size(0), -1)
-        y = self.exit1(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 0.0):
-                return (1, y)
+        x = self.bn1(x)
+        x = self.relu(x)
         x = self.maxpool(x)
-        y = self.avgpool(x)
-        y = y.view(x.size(0), -1)
-        y = self.exit2(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 2.331410984290952e-30):
-                return (2, y)
-        x = self.layer0(x)
-        y = self.avgpool(x)
-        y = y.view(x.size(0), -1)
-        y = self.exit3(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 0.0):
-                return (3, y)
         x = self.layer1(x)
-        y = self.avgpool(x)
-        y = y.view(x.size(0), -1)
-        y = self.exit4(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 1.5324721572728675e-24):
-                return (4, y)
         x = self.layer2(x)
-        y = self.avgpool(x)
-        y = y.view(x.size(0), -1)
-        y = self.exit5(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 0.51512845993042):
-                return (5, y)
         x = self.layer3(x)
-        y = self.avgpool(x)
-        y = y.view(x.size(0), -1)
-        y = self.exit6(y)
-        with torch.no_grad():
-            pk = F.softmax(y, dim=1)
-            entr = -torch.sum(pk * torch.log(pk + 1e-20))
-            if torch.all(entr < 300000):
-                return (6, y)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
 
 
 class HalfResNet(ResNet):
